d9bb28224deeb1b98323bedc754a8693fa5a3b7c
Jeremi Anastaziak <Jeremi Anastaziak>
2017-06-03 17:08:27 +0200

    Intersection visual project

---
+++ bezkolizyjne-skrzyzowanie/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bezkolizyjne_skrzyzowanie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeMyButton();
+        }
+
+        private void InitializeMyButton()
+        {
+            // Create and initialize a Button.
+            Button horizontalRoad = new Button();
+            Button verticalRoad = new Button();
+            int roadWidth = 30;
+            horizontalRoad.Width = ClientRectangle.Width;
+            horizontalRoad.BackColor = Color.DarkGray;
+            horizontalRoad.Top = (ClientRectangle.Height / 2) - (roadWidth/2);
+            horizontalRoad.Height = roadWidth;
+            verticalRoad.Height = ClientRectangle.Height;
+            verticalRoad.BackColor = Color.DarkGray;
+            verticalRoad.Left = (ClientRectangle.Width / 2) - (roadWidth/2);
+            verticalRoad.Width = roadWidth;
+
+            // Add the button to the form.
+            Controls.Add(horizontalRoad);
+            Controls.Add(verticalRoad);
+
+        }
+    }
+}
---
+++ bezkolizyjne-skrzyzowanie/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("bezkolizyjne-skrzyzowanie")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dell Inc.")]
+[assembly: AssemblyProduct("bezkolizyjne-skrzyzowanie")]
+[assembly: AssemblyCopyright("Copyright © Dell Inc. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("8dbfc2f0-6a2b-48f0-84d3-0f0f07b420ba")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
